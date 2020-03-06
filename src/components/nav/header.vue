49bae47ad6c2be4fd40ade32d144c35c3f0d1e14
Dynastiy <emmanuel00michael@gmail.com>
2020-03-06 19:37:59 +0100

    added files

--- src/components/nav/header.vue
+++ src/components/nav/header.vue
@@ -2,7 +2,7 @@
   <div>
     <!-- As a link -->
     <nav class="navbar navbar-light text-center">
-    <a class="navbar-brand text-center" href="#"><img src="src/assets/img/logo.svg" width="20%"></a>
+    <a class="navbar-brand text-center" href="#"><img src="/src/assets/img/logo.svg" width="20%"></a>
     </nav>
 
   </div>
